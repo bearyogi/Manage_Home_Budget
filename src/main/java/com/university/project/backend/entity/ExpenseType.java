a91bb908c9625aed9f4e66da2b00c847cbf65683
Michał Mamla <mmichal1999@gmail.com>
2021-01-14 14:01:52 +0100

    Added getter to Income/Expense type classes.

--- src/main/java/com/university/project/backend/entity/ExpenseType.java
+++ src/main/java/com/university/project/backend/entity/ExpenseType.java
@@ -1,5 +1,8 @@
 package com.university.project.backend.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum ExpenseType {
     TRANSPORT, HEALTH, FAMILY, GROCERIES,
     GIFTS, EDUCATION, HOME, HOBBY
--- src/main/java/com/university/project/backend/entity/IncomeType.java
+++ src/main/java/com/university/project/backend/entity/IncomeType.java
@@ -1,5 +1,8 @@
 package com.university.project.backend.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum IncomeType {
     SALARY, BONUS, GIFT, RETURN
 }
