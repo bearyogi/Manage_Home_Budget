801fd49d1917dd3019db62eedd8eaa8293fb43dc
Michał Mamla <mmichal1999@gmail.com>
2021-01-18 13:09:20 +0100

    Added Expense test

--- src/test/java/com/university/project/entity/BudgetTest.java
+++ src/test/java/com/university/project/entity/BudgetTest.java
@@ -4,15 +4,11 @@ import com.university.project.backend.entity.Budget;
 import com.university.project.backend.entity.Expense;
 import com.university.project.backend.entity.Income;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 @Test(groups = "Entity")
 public class BudgetTest {
@@ -31,9 +27,9 @@ private Budget fullBudget = new Budget();
             testBudget.setIncomes(null);
 
             //Then
-            softAssert.assertEquals(testBudget.getBudgetId(),emptyBudget.getBudgetId(),"Should have null id, but have not.");
-            softAssert.assertEquals(testBudget.getExpenses(),emptyBudget.getExpenses(),"Should have null expenses, but have not.");
-            softAssert.assertEquals(testBudget.getIncomes(),emptyBudget.getIncomes(),"Should have null incomes, but have not.");
+            assertEquals(testBudget.getBudgetId(),emptyBudget.getBudgetId(),"Should have null id, but have not.");
+            assertEquals(testBudget.getExpenses(),emptyBudget.getExpenses(),"Should have null expenses, but have not.");
+            assertEquals(testBudget.getIncomes(),emptyBudget.getIncomes(),"Should have null incomes, but have not.");
         }
 
         @Test
