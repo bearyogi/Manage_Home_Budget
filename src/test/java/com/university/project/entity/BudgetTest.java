38aca49aa1554c164d66d1f37193f7bdf9c58560
Michał Mamla <mmichal1999@gmail.com>
2021-01-18 14:28:40 +0100

    Small changes to test suite

--- src/test/java/com/university/project/entity/BudgetTest.java
+++ src/test/java/com/university/project/entity/BudgetTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.Test;
 import java.util.HashSet;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
-
 @Test(groups = "Entity")
 public class BudgetTest {
 private Budget emptyBudget = new Budget();
